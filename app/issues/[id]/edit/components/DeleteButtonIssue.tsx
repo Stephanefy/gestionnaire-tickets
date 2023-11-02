ce962ff5d3329d63134c0a61d26036b022cbe871
Stephanefy <stephanefokyin@gmail.com>
2023-11-02 10:41:06 +0100

    show a confirmation dialog

--- app/issues/[id]/edit/components/DeleteButtonIssue.tsx
+++ app/issues/[id]/edit/components/DeleteButtonIssue.tsx
@@ -1,14 +1,39 @@
-import { CrossCircledIcon } from '@radix-ui/react-icons'
-import { Button } from '@radix-ui/themes'
-import React from 'react'
+'use client'
 
-const DeleteButtonIssue = ({ issueId } : { issueId: number }) => {
+import { CrossCircledIcon } from "@radix-ui/react-icons";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import React from "react";
+
+const DeleteButtonIssue = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color='red'>
-        <CrossCircledIcon/>
-        Supprimer ce ticket
-    </Button>
-  )
-}
+    <AlertDialog.Root >
+      <AlertDialog.Trigger>
+        <Button color="red" className="cursor-pointer">
+          <CrossCircledIcon />
+          Supprimer ce ticket
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Supprimer ce ticket</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Êtes-vous sûr de vouloir supprimer ce ticket
+        </AlertDialog.Description>
 
-export default DeleteButtonIssue
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Annuler
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Supprimer ce ticket
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
+};
+
+export default DeleteButtonIssue;
