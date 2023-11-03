d4e0bfaaa6fb43f78952b60a4231f4c1d6e5a7a1
Stephanefy <stephanefokyin@gmail.com>
2023-11-03 13:39:48 +0100

    prisma adapter with next auth models

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import Auth0Provider from "next-auth/providers/auth0";
+import prisma from "@/prisma/client";
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID!,
@@ -9,6 +12,9 @@ const handler = NextAuth({
       issuer: process.env.AUTH0_ISSUER,
     }),
   ],
+  session: {
+    strategy: "jwt"
+  }
 });
 
 export { handler as GET, handler as POST };
