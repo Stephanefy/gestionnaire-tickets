26438927cd00a4777126059b6405cfaa41617666
Stephanefy <stephanefokyin@gmail.com>
2023-11-08 12:05:22 +0100

    make column sortable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,31 +2,50 @@ import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "@/app/components";
 import IssueAction from "./IssueAction";
-import { Status } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
+import NextLink from "next/link";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
 interface Props {
-  searchParams: { status: Status}
+  searchParams: { status: Status, orderBy: keyof Issue };
 }
 
-const IssuesPage = async (
-  { searchParams }: Props
-) => {
+const IssuesPage = async ({ searchParams }: Props) => {
+  const columns: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt" },
+  ];
 
-      const statuses = Object.values(Status)
-      const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
-      const issues = await prisma.issue.findMany({
-        where: {
-          status
-        }
-      });
-  
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
 
-
-  return ( 
+  return (
     <div>
       <IssueAction />
       <Table.Root variant="surface">
         <Table.Header>
+          {columns.map((column) => (
+            <Table.ColumnHeaderCell key={column.value}>
+              <Table.Cell className={column.className}>
+               <NextLink href={{
+                query: {...searchParams, orderBy: column.value }
+              }}>{column.label}</NextLink> 
+              {column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
+              </Table.Cell>
+            </Table.ColumnHeaderCell>
+          ))}
           <Table.Row>
             <Table.Cell>Nom du ticket</Table.Cell>
             <Table.Cell className="hidden md:table-cell">
